3560ff3e56ec91a9f697187d6ef9b696be1b5550
Xintong Yu <yxt423@gmail.com>
2014-11-16 15:32:16 -0800

    add tab framework

--- app/src/main/java/com/careerguidance/activity/MainTabHost.java
+++ app/src/main/java/com/careerguidance/activity/MainTabHost.java
@@ -1,36 +1,37 @@
 package com.careerguidance.activity;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.careerguidance.R;
 
+public class MainTabHost extends FragmentActivity implements SubTab.OnFragmentInteractionListener {
 
-public class MainActivity extends Activity {
+    private FragmentTabHost mTabHost;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_tab_host);
+
+        mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+
+        mTabHost.addTab(mTabHost.newTabSpec("Careers").setIndicator("Careers"), SubTab.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("Find Match").setIndicator("Find Match"), SubTab.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("Profile").setIndicator("Profile"), SubTab.class, null);
     }
 
-    public void matchCareerButton(View v) {
-
-    }
-
-    public void careerOptionsButton(View v) {
-        Intent intent = new Intent(this, MainTabHost.class);
-        startActivity(intent);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main_tab_host, menu);
         return true;
     }
 
@@ -40,10 +41,13 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
